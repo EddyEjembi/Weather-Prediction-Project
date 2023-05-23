a5f710a4f856bc3ceacf3bae1f1fed53796b3e24
Eddy Ejembi <eddyejembi2018@gmail.com>
2023-05-23 13:36:04 +0100

    added comments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#Import relevant libraries
 import streamlit as st
 import requests
 import pandas as pd
@@ -8,9 +9,9 @@ import pickle
 from datetime import datetime, timedelta
 
 # API key for weatherapi.com
-API_KEY = 'a62c65107ba54eab825202117231205'
+API_KEY = 'fb222dc231b44f05b0682536230205'
 
-#load machine learning models
+#load machine learning model
 with open('ensemble_model.pkl', 'rb') as file:
     ensemble = pickle.load(file)
 
@@ -18,14 +19,18 @@ with open('ensemble_model.pkl', 'rb') as file:
 @st.cache_data
 def fetch_hourly_weather_data(date, city, lat, lon):
     date = date.strftime('%Y-%m-%d')  # Convert date to string
+    #make API request
     url = 'http://api.weatherapi.com/v1/forecast.json?key=' + API_KEY + '&q=' + str(lat) + ',' + str(lon) + '&dt=' + date + '&hour=0-23'
     response = requests.get(url)
     data = response.json()
     #print(response)
+    #Parameter for API request
     hourly_data = data.get('forecast', {}).get('forecastday', [])[0].get('hour', [])
 
+    #List to store weather data
     weather_data = []
     for hour in hourly_data:
+        #extract relevant features from API
         weather = {
             "city": city,
             "date_time": pd.to_datetime(hour["time"]),
@@ -41,11 +46,14 @@ def fetch_hourly_weather_data(date, city, lat, lon):
             "UV Index": hour.get("uv", "N/A"),
             "condition": hour["condition"]["text"]
         }
+        #Append features to weather data List
         weather_data.append(weather)
 
+    #Return weather data List
     return weather_data
 
 
+#Main app function
 def main():
     st.title("Weather Prediction App")
 
@@ -53,14 +61,16 @@ def main():
 
     # User inputs
     cities = ["Lagos", "Port Harcourt", "Kano", "Abuja", "Ibadan", "Ota"]
+    #Days to forcast
     def get_next_days():
         today = datetime.today().date()
-        dat = pd.date_range(today, periods=7).date
+        dat = pd.date_range(today, periods=3).date
         return dat
 
-    # Fetch the next 7 days
+    # Fetch the next 3 days
     dat = get_next_days()
 
+    #Input fields
     with col1:
         city = st.selectbox("Select the city:", cities)
     with col2:
@@ -68,6 +78,7 @@ def main():
     lat, lon = 0, 0 #latitude and longitude of the city
     city_encode = 0 #encoded value of the city
 
+    #Cities to predict for
     if city and dates:
         if city == "Lagos":
             lat, lon = "6.5244", "3.3792"
@@ -87,11 +98,13 @@ def main():
         elif city == "Ota":
             lat, lon = "6.6804", "3.2356"
             city_encode = 4
-        # Fetch the hourly weather data
+        
+        # Fetch the hourly weather data for dates selected
         for date in dates:
             print(date)
             weather_data = fetch_hourly_weather_data(date, city, lat, lon)
 
+            #If data is fetched
             if weather_data:
                 # Convert the weather data to a pandas DataFrame
                 df = pd.DataFrame(weather_data)
@@ -121,37 +134,49 @@ def main():
                 # Transform the selected features using the scaler
                 df[features] = scaler.transform(df[features])
 
-                # Make predictions using the ensemble model
+                # Divide the Day into 4 Sections
                 section_data = df
                 section_data['Section of Day'] = pd.cut(df['hour'], bins=[0, 6, 12, 18, 24], labels=['Night', 'Morning', 'Afternoon', 'Evening'])
                 #print(section_data)
 
+                #Create 4 different dataframe for each section
                 night = section_data[section_data['Section of Day'] == 'Night']
                 morning = section_data[section_data['Section of Day'] == 'Morning']
                 afternoon = section_data[section_data['Section of Day'] == 'Afternoon']
                 evening = section_data[section_data['Section of Day'] == 'Evening']
                 #print(night)
+                #Compute the aggregate for each section
                 features_2 = ['hour', 'hour_sin', 'hour_cos']
                 night[features + features_2] = night[features + features_2].mean()
                 morning[features + features_2] = morning[features +features_2].mean()
                 afternoon[features + features_2] = afternoon[features + features_2].mean()
                 evening[features + features_2] = evening[features + features_2].mean()
                 #print(night)
+                #variable to hold sections data
                 section = [night, morning, afternoon, evening]
+                #List to store result of section prediction
                 section_result = []
+
                 #prediction for section data
                 for i in section:
+                    #Drop the columns not needed in prediction
                     x = i.drop(['condition', 'city', 'date_time', 'year', 'Section of Day'], axis=1)
+                    #Make prediction
                     pred = ensemble.predict(x)
+                    #Get the variable name of the section and store it as a string
                     var_name = [name for name, value in locals().items() if value is i][0]
                     result = [var_name.capitalize(), pred.mean()]
+                    #Append the section and prediction to the section result List
                     section_result.append(result)
                 print(section_result)
 
                 #prdiction for hourly data
+                #Drop the columns not needed in prediction
                 data = df.drop(['condition', 'city', 'date_time', 'year', 'Section of Day'], axis=1)
+                #Make prediction for every hour of the day
                 predictions = ensemble.predict(data)
 
+                #Dictionary to decode the Weather Conditions
                 decoded_condition = {
                     'Clear': 0,
                     'Cloudy': 1,
@@ -173,8 +198,10 @@ def main():
                 section_data['Predicted Condition Encoded'] = pd.DataFrame(predictions)
 
                 pred = None
+                #List to hold the decoded Predictions for every hour
                 decode = []
 
+                #Decode the condition and append it to the List
                 for i in predictions:
                     for key, value in decoded_condition.items():
                         if value == i:
@@ -185,6 +212,7 @@ def main():
                 st.write("")
                 st.header("Predicted Weather Condition for {}:".format(date))
                 #st.write('At {} (00:00 - 05:00) expect a \"{}\" Weather'.format(dis, pred.upper()))
+                #Loop through the section result and display the section along with it's prediction
                 for i in section_result:
                     for key, value in decoded_condition.items():
                         if value == i[1]:
@@ -199,8 +227,9 @@ def main():
                     if i[0] == 'Evening':
                         st.write('In the {} (18:00 - 23:00) expect a \"{}\" Weather'.format(i[0], pred.upper()))
                 
+                #Display the predictions
                 section_data['Predicted Condition'] = decode
-                # Display the predictions
+
                 st.subheader("Predicted Weather Conditions Every hour:")
                 columns_to_not_display = ['condition', 'city', 'date_time', 'city_encoded', 'year', 'month', 'day', 'hour',
                                          'month_sin', 'month_cos', 'hour_sin', 'hour_cos', 'Predicted Condition Encoded']
